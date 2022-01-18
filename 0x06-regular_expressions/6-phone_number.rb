ce70a816931e889db6c6493d66f4aa0e21cd22fa
Tonycrz <tonybaidoo0@gmail.com>
2022-01-18 15:05:31 +0000

    specifically matches 10 digits

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
 # regular expression must match a 10-digit phone number
 
-ARGV[0].scan(/^\d{10}$/} {|match| print match}
+ARGV[0].scan(/^\d{10}$/) {|match| print match}
+print "\n"
