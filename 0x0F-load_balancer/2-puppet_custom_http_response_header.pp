892c168144e69047251989214252eedc5aad8cba
Tonycrz <tonybaidoo0@gmail.com>
2022-03-21 15:47:03 +0000

    2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,29 @@
+# creating a custom HTTP header response, but with Puppet
+# Puppet manifest to install nginx
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file{ 'add custom header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'location / {',
+  line   => 'add_header X-Served-By $hostname;',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
