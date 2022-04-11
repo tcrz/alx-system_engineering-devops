6421987484b8cf342464d929071a77b412e7d38e
Tonycrz <tonybaidoo0@gmail.com>
2022-04-11 13:19:40 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,13 +9,13 @@ if __name__ == "__main__":
     user_req = requests.get("https://jsonplaceholder.typicode.com/users/{}".
                             format(sys.argv[1]))
     user_name = user_req.json().get("name")
-    tasks_req = requests.get("""https://jsonplaceholder.typicode.com/todos?
-                             userId={}""".format(sys.argv[1]))
-    total_tasks = len(tasks_req.json())
+    tasks_req = requests.get("https://jsonplaceholder.typicode.com/todos")
+    total_tasks = 0
     cmp_tasks = 0
     cmp_tasks_desc = ""
     for each in tasks_req.json():
         if each["userId"] == int(sys.argv[1]):
+            total_tasks += 1
             if each["completed"] is True:
                 cmp_tasks += 1
                 cmp_tasks_desc += "\t {}\n".format(each["title"])
