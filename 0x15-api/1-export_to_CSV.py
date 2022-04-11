b7b88518d22282c0e1928521146ccd365d973108
Tonycrz <tonybaidoo0@gmail.com>
2022-04-11 19:42:45 +0000

    update_docstring

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""gathers data from an API and exports it to CSV file"""
+"""
+Gathers data from an API and exports it to CSV file
+Records all tasks that are owned by this employee
+Format must be: 
+"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
+"""
 
 
 if __name__ == "__main__":
