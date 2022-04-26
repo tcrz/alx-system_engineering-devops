af7472cd9f6737319d8567e7980d13960c056df4
Tonycrz <tonybaidoo0@gmail.com>
2022-04-26 12:42:19 +0000

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API and returns the number of subscribers
+    for a given subreddit"""
+    r = requests.get(
+                    "https://www.reddit.com/r/{}/about.json".format(subreddit),
+                    allow_redirects=False,
+                    headers={'User-Agent': 'My User Agent 1.0'})
+    if r.status_code == 200:
+        data = r.json()
+        return data["data"]["subscribers"]
+    return 0
+
+# Uncomment to Test function:
+# number_of_subscribers = __import__('0-subs').number_of_subscribers
+# if len(sys.argv) < 2:
+#     print("Please pass an argument for the subreddit to search.")
+# else:
+#     print("{:d}".format(number_of_subscribers(sys.argv[1])))
