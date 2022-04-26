4ce7a794b0757da26b3b7dca40e83148a193ac61
Tonycrz <tonybaidoo0@gmail.com>
2022-04-26 22:13:26 +0000

    100-count.py

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+recursive function that queries the Reddit API and returns a list
+containing the titles of the all hot posts recursively
+"""
+import requests
+
+
+def count_words(subreddit, word_list, hot_list=[]):
+    """sets base url and calls recursive function to return
+    list of all titles"""
+    word_dict = dict.fromkeys(word_list, 0)
+    url = "https://www.reddit.com/r/{}/hot.json?after=".format(subreddit)
+    r = requests.get(url, allow_redirects=False,
+                     headers={'User-Agent': 'My User Agent 1.0'})
+    if r.status_code == 200:
+        data = r.json()
+        after = data["data"]["after"]
+        for child in data["data"]["children"]:
+            hot_list.append(child["data"]["title"])
+        getpage(subreddit, url, after, hot_list)
+        for key in word_dict:
+            for titles in hot_list:
+                for word in titles.split():
+                    if key.casefold() == word.casefold():
+                        word_dict[key] += 1
+        print(word_dict)
+    else:
+        return None
+
+
+def getpage(subreddit, url, after, hot_list=[]):
+    """recursively retrieves data on next page if available and
+    stores data in a list"""
+    next_page = url + after
+    r = requests.get(next_page + "&limit=100", allow_redirects=False,
+                     headers={'User-Agent': 'My User Agent 1.0'})
+    data = r.json()
+    next = data["data"]["after"]
+    for child in data["data"]["children"]:
+        hot_list.append(child["data"]["title"])
+    if next is not None:
+        getpage(subreddit, url, next, hot_list)
+    return
+
+
+# Uncomment to Test function:
+# import sys
+# count_words = __import__('100-count').count_words
+# if len(sys.argv) < 3:
+#     print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+#     print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+# else:
+#     result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
