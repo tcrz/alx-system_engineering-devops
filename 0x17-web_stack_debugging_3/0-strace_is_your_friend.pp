4e4b9a9ab56bf6dece0deff65861a7f64beb11b7
Tonycrz <tonybaidoo0@gmail.com>
2022-04-27 16:32:39 +0000

    add extra paths

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,3 @@
 exec { 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp':
-path => ['/usr/bin', '/usr/sbin'],
+path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
